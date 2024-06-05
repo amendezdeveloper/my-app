13743ed0ce7247e5fc022365cdfedf8af8c4ff06
amendezdeveloper <amendez.developer@gmail.com>
2024-06-04 23:33:01 -0600

    Release 0.2 for tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    RouterLink
+    RouterLink,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
+import { StickyComponent } from '../sticky/sticky.component';
+import { CardsComponent } from '../cards/cards.component';
+import { FeaturesComponent } from '../features/features.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ContactFormComponent],
+  imports: [ContactFormComponent,
+    StickyComponent,
+    CardsComponent,
+    FeaturesComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
